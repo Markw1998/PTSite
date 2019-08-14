fbb73cc9413b953b93b0cf88c5e4d5c4c5f34fa6
Markw1998 <35930598+Markw1998@users.noreply.github.com>
2019-08-14 16:03:07 +0100

    Contact Form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './comp/contact/contact.component';
+import { MainComponent } from './comp/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contact', component: ContactComponent },
+  { path: '', component: MainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './comp/main/main.component';
+import { ContactComponent } from './comp/contact/contact.component';
+import { NavbarComponent } from './comp/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
